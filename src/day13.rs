2cfffa39c0020d13e32b5e6f6edc319027b01b62
Connor <connortalbott21@gmail.com>
2024-12-13 16:23:52 -0700

    yay, Im so smart

--- src/day13.rs
+++ src/day13.rs
@@ -16,34 +16,37 @@ fn main() {
     //[r2_x, r2_y] [r2_s]
     let mut tot = 0;
     for i in {0..file.len()}.step_by(4) {
-        
+        //14932 ish to 32966
         let mut sols = vec![get_nums(file[i])]; 
         sols.push(get_nums(file[i+1]));
-        let sol = get_nums(file[i+2]); // solutions
+        let mut sol = get_nums(file[i+2]); // solutions
+        sol[0] = sol[0] + 10000000000000;
+        sol[1] = sol[1] + 10000000000000;
         let d = det(sols.clone()); 
         let mx = vec![vec![sol[0], sols[1][0]], vec![sol[1], sols[1][1]]];
         let dx = det(mx); // the borrow checker is throwing a fit
         let my = vec![vec![sols[0][0], sol[0]], vec![sols[0][1], sol[1]]];
         let dy = det(my);
-        let x = dx / d;
-        let y = dy / d;
-        if x < 0 || y < 0 {continue;}
-        if x > 100 || y > 100 {continue;}
+        let x = dx as f64 / d as f64;
+        let y = dy as f64 / d as f64;
+        //if x < 0.0 || y < 0.0 {continue;}
+        //if x > 100.0 || y > 100.0 {continue;}
+        if x % 1.0 != 0.0 || y % 1.0 != 0.0 {continue;}
         println!("{:?}", x);
         println!("{:?}", y);
-        tot += x*3;
-        tot += y;
+        tot += 3*x as i128;
+        tot += y as i128;
         println!("{}", tot);
     }
     println!("{}", tot);
     println!("{:?}", start.elapsed());
 }
 
-fn det(mx: Vec<Vec<i32>>) -> i32 {
+fn det(mx: Vec<Vec<i128>>) -> i128 {
     mx[0][0] * mx[1][1] - mx[0][1] * mx[1][0]
 }
 
-fn get_nums(line: &str) -> Vec<i32> {
+fn get_nums(line: &str) -> Vec<i128> {
     let re = Regex::new(r"(\d+)").unwrap();
     re.captures_iter(line).map(|x| x[1].parse().unwrap()).collect() 
 }
